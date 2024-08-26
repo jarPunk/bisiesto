91ef951d9e3c145ca4f36d0a3f42a124338998d4
jarPunk <jar_ax@hotmail.com>
2024-08-26 17:36:57 -0400

    Tercer Ciclo

--- src/year.test.js
+++ src/year.test.js
@@ -7,3 +7,6 @@ test('Año divisible por 400 es bisiesto', () => {
 test('Año divisible por 100 pero no por 400 NO es bisiesto', () => {
     expect(isLeapYear(1900)).toBe(false);
 });
+test('Año divisible por 4 pero no por 100 es bisiesto', () => {
+    expect(isLeapYear(2008)).toBe(true);
+});
